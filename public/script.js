a3dd3e4d41de6f6fec7fb7974df9104b128ff280
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2022-08-12 14:55:58 +0530

    Fourth Commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,73 @@
+// const socket = io("/");
+//
+// var peer = new Peer(undefined, {
+//   path: "/peerjs",
+//   host: "/",
+//   port: "443",
+// });
+//
+// const myVideo = document.createElement("video");
+// myVideo.muted = true;
+// navigator.mediaDevices
+//   .getUserMedia({ audio: true, video: false })
+//   .then((stream) => {
+//     addVideoStream(myVideo, stream);
+//     socket.on("user-connected", (uid) => {
+//       connectToNewUser(uid, stream);
+//     });
+//     peer.on("call", (call) => {
+//       call.answer(stream);
+//       const video = document.createElement("video");
+//       call.on("stream", (userVideoStream) => {
+//         addVideoStream(video, userVideoStream);
+//       });
+//     });
+//   });
+//
+// function connectToNewUser(userId, stream) {
+//   const call = peer.call(userId, stream);
+//   const video = document.createElement("video");
+//   call.on("stream", (userVideoStream) => {
+//     addVideoStream(video, userVideoStream);
+//   });
+// }
+//
+// let myStream;
+//
+// function addVideoStream(video, stream) {
+//   video.srcObject = stream;
+//   video.addEventListener("loadedmetadata", () => {
+//     video.play();
+//     let html = `
+//             <div class="user-container">
+//                 ${video.outerHTML}
+//             </div>
+//         `;
+//     $("#users").append(html);
+//   });
+// }
+//
+// $(function () {
+//   $("#mute_button").click(function () {
+//     const enabled = myStream.getAudioTracks()[0].enabled;
+//     if (enabled) {
+//       myStream.getAudioTracks()[0].enabled = false;
+//       html = `<i class="fas fa-microphone-slash"></i>`;
+//       $("#mute_button").toggleClass("background_red");
+//       $("#mute_button").html(html);
+//     } else {
+//       myStream.getAudioTracks()[0].enabled = true;
+//       html = `<i class="fas fa-microphone"></i>`;
+//       $("#mute_button").toggleClass("background_red");
+//       $("#mute_button").html(html);
+//     }
+//   });
+// });
+//
+// peer.on("open", (id) => {
+//   socket.emit("join-room", ROOM_ID, id);
+// });
+
 const socket = io("/");
 
 var peer = new Peer(undefined, {
@@ -8,13 +78,22 @@ var peer = new Peer(undefined, {
 
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+let myStream;
+
 navigator.mediaDevices
-  .getUserMedia({ audio: true, video: false })
+  .getUserMedia({
+    audio: true,
+    video: false,
+  })
   .then((stream) => {
+    myStream = stream;
     addVideoStream(myVideo, stream);
-    socket.on("user-connected", (uid) => {
-      connectToNewUser(uid, stream);
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
     });
+
     peer.on("call", (call) => {
       call.answer(stream);
       const video = document.createElement("video");
@@ -31,8 +110,6 @@ function connectToNewUser(userId, stream) {
     addVideoStream(video, userVideoStream);
   });
 }
-
-let myStream;
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
